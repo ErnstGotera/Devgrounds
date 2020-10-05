4d2c21642da032a424658e5cb55387740321433e
ernstgotera <ernestogl5@hotmail.com>
2020-10-04 21:13:58 -0400

    start creating the folder structure, initial components, add assets folder and hero image of the homepage, change the default create react app index.html text for the devgrounds one

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Navbar from 'components/Navbar';
+import HomePage from 'pages/HomePage';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+const App = () => {
+  <Fragment className="App">
+    <Navbar />
+    <HomePage />
+  </Fragment>;
+};
 
 export default App;
