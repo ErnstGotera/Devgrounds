d7fe72003252fb1a90619d98289b8e3b3f91d848
ernstgotera <ernestogl5@hotmail.com>
2021-07-05 18:05:01 -0400

    add searchbar to posts, add socket.io in client and server

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Footer.scss';
 const Footer = () => {
-  return <footer className="footer bg-dark">Placeholder Footer</footer>;
+  return <footer className="footer bg-dark">{''}</footer>;
 };
 
 export default Footer;
--- client/src/pages/Posts/Posts.jsx
+++ client/src/pages/Posts/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import PostItem from './PostItem';
@@ -7,9 +7,16 @@ import { getPosts } from '../../redux/actions/post';
 import { FaUser } from 'react-icons/fa';
 
 const Posts = ({ getPosts, post: { posts } }) => {
+  const [search, setSearch] = useState('');
   useEffect(() => {
     getPosts();
   }, [getPosts]);
+
+  const filteredPosts = posts.filter(
+    post =>
+      post.name.toLowerCase().includes(search.toLowerCase()) ||
+      post.text.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <Fragment>
@@ -18,8 +25,14 @@ const Posts = ({ getPosts, post: { posts } }) => {
         <FaUser /> Welcome to the community
       </p>
       <PostForm />
+      <input
+        onChange={e => setSearch(e.target.value)}
+        type="search"
+        placeholder="Search posts"
+        className="search-bar"
+      />
       <div className="posts">
-        {posts.map(post => (
+        {filteredPosts.map(post => (
           <PostItem key={post._id} post={post} />
         ))}
       </div>
