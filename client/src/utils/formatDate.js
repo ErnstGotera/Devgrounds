cc74cc2d36c7c15f6ab8c4815acb106fe9b530f3
ernstgotera <ernestogl5@hotmail.com>
2021-07-09 17:27:01 -0400

    fixes to svg icons alignment, make navbar responsive, remove formatIso from date utils, update packages and remove unused dependencies

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -2,11 +2,4 @@ function formatDate(date) {
   return new Intl.DateTimeFormat().format(new Date(date));
 }
 
-export const formatIso = date => {
-  return new Intl.DateTimeFormat('en-GB', {
-    dateStyle: 'full',
-    timeStyle: 'short'
-  }).format(new Date(date));
-};
-
 export default formatDate;
